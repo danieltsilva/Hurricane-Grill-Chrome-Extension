75457381274bb43e30ec7fc1eff614664e18dcf3
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-03-19 09:39:55 +1100

    Adding a complex event

--- oauth.js
+++ oauth.js
@@ -8,17 +8,20 @@ var API_KEY = 'AIzaSyDsjIwrvc61c2Ckm6rHPSBNRoRBim_yw44';
 var CALENDAR_ID_PRIMARY = 'eugostodequeijadinhadequeijo@gmail.com';
 var CALENDAR_ID_WORK = '832fm0rrd8pr869c4e7vnhlh8c@group.calendar.google.com';
 
+var TIMEZONE_SYDNEY = 'Australia/Sydney';
+var TIMEZONE_SAOPAULO = 'America/Sao_Paulo';
+
 var event = {
   'summary': 'Google I/O 2015',
   'location': '800 Howard St., San Francisco, CA 94103',
   'description': 'A chance to hear more about Google\'s developer products.',
   'start': {
-    'dateTime': '2018-12-01T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
+    'dateTime': '2019-03-16T09:00:00-07:00',
+    'timeZone': TIMEZONE_SYDNEY
   },
   'end': {
-    'dateTime': '2015-05-28T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
+    'dateTime': '2019-03-28T17:00:00-07:00',
+    'timeZone': TIMEZONE_SYDNEY
   },
   'recurrence': [
     'RRULE:FREQ=DAILY;COUNT=2'
@@ -46,10 +49,6 @@ var makeQuerystring = params =>
 window.onload = function() {
   document.querySelector('button').addEventListener('click', function() {
     chrome.identity.getAuthToken({interactive: true}, function(token) {
-      console.log(token);
-      console.log(makeQuerystring(event));
-      console.log('I say a little pray for yooouuuu!')
-
       let init = {
         method: 'POST',
         async: true,
@@ -57,10 +56,11 @@ window.onload = function() {
           Authorization: 'Bearer ' + token,
           'Content-Type': 'application/json'
         },
+        body: JSON.stringify(event),
         'contentType': 'json'
       };
       fetch(
-          'https://www.googleapis.com/calendar/v3/calendars/' + encodeURIComponent(CALENDAR_ID_WORK) + '/events/quickAdd?' + 'text=Textandiu' + '&key=' + API_KEY,
+          'https://www.googleapis.com/calendar/v3/calendars/' + encodeURIComponent(CALENDAR_ID_WORK) + '/events/?key=' + API_KEY,
           init)
           .then((response) => response.json())
           .then(function(data) {
